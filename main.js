97f31a96a044c187bf96e400233998fc20804541
HichamOujari <hichamoujari99@gmail.com>
2020-12-02 18:58:03 +0100

    committed by hicham

--- main.js
+++ main.js
@@ -5,8 +5,6 @@
 for(i=1;i<nbr;i++){
 	document.getElementById("20")
 }*/
-
-
 function day_num(day,month,year){
 	var date =new Date(month+'-'+day+'-'+year);
 	switch(date.toString().split(' ')[0]){
@@ -24,6 +22,9 @@ function day_num(day,month,year){
 function fill(month,year){
 	var start =day_num(1,month,year);
 	var end = new Date(year, month, 0).getDate();
+	for(i=1;i<=35;i++){
+		document.getElementById(i).innerHTML="";
+	}
 	for(i=1;i<=end;i++){
 		//if(i.isOccupe){
 			//document.getElementById(i+start-1).className+=" occuped";
@@ -32,4 +33,20 @@ function fill(month,year){
 	}
 }
 
-fill(11,2020);
+function init(){
+	var d = new Date();
+
+	var ele =document.getElementById("month");
+	ele.value=d.getMonth()+1;
+	ele.addEventListener("change",(e)=>{
+		fill(parseInt(e.target.value),parseInt(document.getElementById("years").value));
+	})
+	ele=document.getElementById("years");
+	ele.addEventListener("change",(e)=>{
+		fill(parseInt(document.getElementById("month").value),parseInt(e.target.value));
+	})
+	ele.value=d.getFullYear();
+	fill(d.getMonth()+1,d.getFullYear());
+}
+
+init();
